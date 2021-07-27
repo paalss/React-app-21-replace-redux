908b34bf3cfa99d4ae00c7e4e2a5353aeaf5ad33
paalss <paalss94@gmail.com>
2021-07-27 11:12:26 +0200

    state manages nå med context api. erstatter redux

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -32,15 +32,7 @@ const initialState = {
 const productReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_FAV:
-      const prodIndex = state.products.findIndex(
-        p => p.id === action.productId
-      );
-      const newFavStatus = !state.products[prodIndex].isFavorite;
-      const updatedProducts = [...state.products];
-      updatedProducts[prodIndex] = {
-        ...state.products[prodIndex],
-        isFavorite: newFavStatus
-      };
+      
       return {
         ...state,
         products: updatedProducts
